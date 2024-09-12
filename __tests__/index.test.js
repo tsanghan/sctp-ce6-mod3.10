82bf71073eb983dccee17035a78e22baf32dcc83
Wong Tsang Han <tsanghan@gmail.com>
2024-09-12 21:02:52 +0800

    10th commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,34 @@
+// functions.test.js
+
+const { hello, hello2, hello3 } = require('../index'); // Adjust the path to where your functions are defined
+
+describe('Test hello functions', () => {
+
+  test('hello function should return status code 200 and correct message', async () => {
+    const result = await hello();
+
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toBe(JSON.stringify({
+      message: 'Go Serverless v4.0! Your function executed successfully!'
+    }));
+  });
+
+  test('hello2 function should return status code 200 and correct message', async () => {
+    const result = await hello2();
+
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toBe(JSON.stringify({
+      message: 'Go Serverless v4.0! Your function executed successfully! Function 2'
+    }));
+  });
+
+  test('hello3 function should return status code 200 and correct message', async () => {
+    const result = await hello3();
+
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toBe(JSON.stringify({
+      message: 'Go Serverless v4.0! Your function executed successfully! Function 3'
+    }));
+  });
+
+});
