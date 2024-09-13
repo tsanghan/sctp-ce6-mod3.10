cda2fbf00ecd9494c28f0b0dd150eebb1cd7709c
Wong Tsang Han <tsanghan@gmail.com>
2024-09-13 21:11:43 +0800

    37th commit

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+import { PublishCommand } from "@aws-sdk/client-sns";
+import { snsClient } from "libs/snsClient.js";
+
 exports.hello = async (event) => {
   console.log("*****HELLO*****")
+  const publish = async (
+    message = "Hello from SNS!",
+    topicArn = "arn:aws:sns:ap-southeast-1:255945442255:MyCustomTopic-node",
+  ) => {
+    const response = await snsClient.send(
+      new PublishCommand({
+        Message: message,
+        TopicArn: topicArn,
+      }),
+    );
   return {
     statusCode: 200,
     body: JSON.stringify({
